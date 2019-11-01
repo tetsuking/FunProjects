92f59d434916e995b3a23245f23545424df478e4
Alex Donyush <donyush@list.ru>
2019-11-01 13:25:33 +0300

    yay

---
+++ aneks.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import base_class
+import requests
+import lxml.html
+class acb(base_class.public_command):
+    """docstring for ."""
+    #html_tree = ""
+    command = "!анек"
+    resourse = ""
+    anek =""
+    def __init__(self, url="https://baneks.ru/random"):
+        self.url = url
+
+    def get_anek (self):
+        self.anek = ""
+        r = requests.get(self.url)
+        html = lxml.html.fromstring(r.content)
+        title = html.xpath('.//article//p//text()')
+        with open('fit_ha.txt', 'w', encoding='utf-8') as f:
+            for text in title:
+                self.anek += text
+        return self.anek
+
+    def load(self):
+        return {"command": self.get_anek}
+
+    def exec(self):
+        print ("lol")
+
+
+def main():
+    p = acb()
+    p.get_anek()
+
+if __name__ == '__main__':
+    main()
